b4cbd67d551116ab692829c6f1a482c4aa54101d
Z1g1e <dulancij@gmail.com>
2024-12-21 22:33:40 +0200

    first

---
+++ project/index.js
@@ -0,0 +1,15 @@
+document.getElementById("loginButton").addEventListener("click", function() {
+    // Редірект на сторінку для входу
+    window.location.href = "#";
+});
+
+document.getElementById("signupButton").addEventListener("click", function() {
+    // Редірект на сторінку реєстрації
+    window.location.href = "#";
+});
+
+document.getElementById("startdaystrial").addEventListener("click", function() {
+    // Редірект на сторінку реєстрації
+    window.location.href = "#";
+});
+
